1d9896b5a4847eacf56396a862c43c0751bb81ac
Edwin Low <edwin.lowxh@gmail.com>
2023-02-06 01:11:48 +0800

    Created new user_profile application

--- FinApp/Main/admin.py
+++ FinApp/Main/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from Main.models import User, UserInformation
+from Main.models import User
 
 admin.site.register(User)  # Show accounts on admin console
-admin.site.register(UserInformation)
