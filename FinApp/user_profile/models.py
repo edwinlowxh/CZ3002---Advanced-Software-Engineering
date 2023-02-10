fcd408ba63dc68fdb6d744f09701c72b9d42f2c6
Edwin Low <edwin.lowxh@gmail.com>
2023-02-11 02:03:42 +0800

    Added user information manager

--- FinApp/user_profile/models.py
+++ FinApp/user_profile/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from user_profile.manager import UserInformationManager
+
 # Create your models here.
 class UserInformation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField()
     marital_status = models.CharField(max_length=7)
+    user_information_manager = UserInformationManager()
 
     
