68e8d021d576bcf85f8092236498698b0d9b4d7f
Low Ren Hwa <renhwa@MacBookAirLOWRENHWA.local>
2023-02-19 12:23:17 +0800

    created budget and category manager

---
+++ FinApp/Budget/manager/CategoryManager.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.db import models
+
+from django.contrib.auth.models import User 
+
+from django.utils.timezone import datetime
+
+if TYPE_CHECKING:
+    from Budget.models import Category
+
+class CategoryManager(models.Manager):
+    def retrieve_catergory(self, **kwargs) -> models.QuerySet:
+        user = kwargs['user']
+
+        return super().get_queryset().filter(
+            user=user
+        )
+    
+    def create_catergory(self, user: User, name: str) -> Category:
+        return super().create(
+            user=user,
+            name = name
+        )
+
+    def delete_catergory(self, id: int) -> None:
+        super().update(is_active = False)
+        
+
+    def update_catergory(self, id: int, user: User, name: str, is_active: bool) -> Category:
+        return super().update(
+            id=id,
+            user=user,
+            name = name,
+            is_active = is_active
+        )
