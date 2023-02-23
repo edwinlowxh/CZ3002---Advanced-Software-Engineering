1142e99a43c484e4c09532397e59f90525e49907
Edwin Low <edwin.lowxh@gmail.com>
2023-02-23 13:10:57 +0800

    Added form checks to user profile

--- FinApp/user_profile/models.py
+++ FinApp/user_profile/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from user_profile.manager import UserInformationManager
+from user_profile.managers.UserInformationManager import UserInformationManager
 
 # Create your models here.
 class UserInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birth_date = models.DateField()
-    marital_status = models.CharField(max_length=7)
+    date_of_birth = models.DateField(blank=True, null=True)
+    marital_status = models.CharField(null=True, max_length=7)
     user_information_manager = UserInformationManager()
 
     
