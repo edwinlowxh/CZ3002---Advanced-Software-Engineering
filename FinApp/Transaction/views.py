2923b1fb653cde2a2efafb2ba41233aa2113c586
Edwin Low <edwin.lowxh@gmail.com>
2023-02-25 00:44:19 +0800

    Added transaction page

--- FinApp/Transaction/views.py
+++ FinApp/Transaction/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Transaction
 
@@ -15,6 +15,8 @@ from .constants import (
     START_DATE_QUERY_PARAM,
     END_DATE_QUERY_PARAM,
     TRANSACTION_ID_VAR,
+    INCOME_TABLE_HEADER,
+    EXPENSE_TABLE_HEADER
 )
 
 from Budget.models import(
@@ -23,25 +25,36 @@ from Budget.models import(
 
 # Create your views here.
 @csrf_exempt
-@basic_auth
+# @basic_auth
 def get_transactions(request, start_date: str = None, end_date: str = None):
     if request.user.is_authenticated:
         if request.method == 'GET':
             start_date = request.GET.get(START_DATE_QUERY_PARAM, None)
-            end_date = request.GET.get(END_DATE_QUERY_PARAM, None)            
+            end_date = request.GET.get(END_DATE_QUERY_PARAM, None)
+            context = {'income_table_header': INCOME_TABLE_HEADER, 'expense_table_header': EXPENSE_TABLE_HEADER}            
 
             if not start_date and not end_date:
                 query_set = Transaction.transaction_manager.retrieve_transaction(user=request.user)
-                return JsonResponse({'range': 'Any', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
+                context['range'] = 'Any'
+                # return JsonResponse({'range': 'Any', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
             elif not start_date:
                 query_set = Transaction.transaction_manager.retrieve_transaction(user=request.user, end_date=tuple(end_date.split('-')))
-                return JsonResponse({'range': f'Before {end_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
+                context['range'] = f'Before {end_date}'
+                # return JsonResponse({'range': f'Before {end_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
             elif not end_date:
                 query_set = Transaction.transaction_manager.retrieve_transaction(user=request.user, start_date=tuple(start_date.split('-')))
-                return JsonResponse({'range': f'After {start_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
+                context['range'] = f'After {start_date}'
+                # return JsonResponse({'range': f'After {start_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
             else:
                 query_set = Transaction.transaction_manager.retrieve_transaction(user=request.user, start_date=tuple(start_date.split('-')), end_date=tuple(end_date.split('-')))
-                return JsonResponse({'range': f'From {start_date} to {end_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
+                context['range'] = f'From {start_date} to {end_date}'
+                # return JsonResponse({'range': f'From {start_date} to {end_date}', 'transactions': [model_to_dict(transaction) for transaction in query_set]})
+                
+            context['transactions'] = [model_to_dict(transaction) for transaction in query_set]
+            print(request.user, context)
+            return render(request, 'transaction.html', context)
+    else:
+        return redirect('/profile/login')
 
 @csrf_exempt
 @basic_auth
