58a61fca83db6a7b4a1b53a3ca81f9e6ba69fade
Ren Hwa <lowrenhwa88@gmail.com>
2023-02-27 19:53:01 +0800

    Added form and view for category

--- FinApp/Budget/views.py
+++ FinApp/Budget/views.py
@@ -13,8 +13,12 @@ from FinApp.decorators import basic_auth
 from .models import Category, Budget
 
 from .constants import(
-    CATEGORY_NAME_VAR
+    CATEGORY_NAME_VAR,
+    CATEGORY_ID_VAR
+
 )
+
+from .forms.CreateCategoryForm import CreateCategoryForm
 
 # Create your views here.
 
@@ -33,8 +37,72 @@ def create_category(request):
             else:
                 return JsonResponse(data={'message': 'Please provide a name for your category'})
 
+@csrf_exempt
+@basic_auth
+def create_category(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form_data = CreateCategoryForm.map_json(request.POST.dict())
+            form_data['user'] = request.user
+            form = CreateCategoryForm(form_data)
             
+            if form.is_valid():
+                print(form.cleaned_data)
+                new_category = Category.category_manager.create_category(
+                    **form.cleaned_data
+                )
+                return JsonResponse(model_to_dict(new_category))
+            else:
+                return JsonResponse({'message': 'Failed to create category', 'errors': form.errors})
+
+            # except Exception as e:
+            #     return JsonResponse({'message': 'Failed to create new transaction'})
+        elif request.method == 'GET':
+            categories = Category.category_manager.get_categories(user=request.user, is_active = True)
+            return JsonResponse({'categories': [model_to_dict(category) for category in categories]})
             
+
+@csrf_exempt
+@basic_auth
+def delete_category(request):
+     if request.user.is_authenticated:
+        if request.method == 'POST':
+            category_id = request.POST.get(CATEGORY_ID_VAR)
+            try:
+                Category.category_manager.delete_category(user=request.user, id=category_id)
+                return JsonResponse({'message': 'Category Deleted'})
+            except Exception as e:                
+                return JsonResponse({'message': 'Failed to delete category'})
+
+@csrf_exempt
+@basic_auth
+def update_category(request):
+     if request.user.is_authenticated:
+        if request.method == 'POST':
+            category_id = request.POST.get(CATEGORY_ID_VAR)
+
+            try:
+                form_data = CreateCategoryForm.map_json(request.POST.dict())
+                form_data['user'] = request.user
+                form = CreateCategoryForm(form_data)
+                
+                if form.is_valid():
+                    print(form.cleaned_data)
+                    updated_category =  Category.category_manager.update_category(
+                        id=category_id,
+                        **form.cleaned_data
+                    )
+
+                    if not updated_category:
+                        return JsonResponse({'message': 'Failed to update category', 'errors': 'Category does not exist'})
+                    else:
+                        return JsonResponse(model_to_dict(updated_category))
+                else:
+                    return JsonResponse({'message': 'Failed to update category', 'errors': form.errors})
+
+            except Exception as e:           
+                return JsonResponse({'message': 'Failed to update category'})
+           
 
 
 
