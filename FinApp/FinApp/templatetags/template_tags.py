102fdfc7ed7cb2602db11a5cc0a9606c57e46d0c
Edwin Low <edwin.lowxh@gmail.com>
2023-03-02 16:38:50 +0800

    Added model_to_dict template tags

--- FinApp/FinApp/templatetags/template_tags.py
+++ FinApp/FinApp/templatetags/template_tags.py
@@ -1,7 +1,12 @@
 from django import template
+from django.forms import model_to_dict
 
 register = template.Library()
 
 @register.filter(name='get')
 def get(d, key):
-    return d[key]
+    return dict(d)[key]
+
+@register.filter(name='model_to_dict')
+def _model_to_dict(obj):
+    return model_to_dict(obj)
