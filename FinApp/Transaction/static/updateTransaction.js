4369bf8cdb51a9aaa4a5b39604e22ba0c859de8f
Edwin Low <edwin.lowxh@gmail.com>
2023-03-12 01:36:45 +0800

    Updated Transasction

--- FinApp/Transaction/static/updateTransaction.js
+++ FinApp/Transaction/static/updateTransaction.js
@@ -1,4 +1,4 @@
-import { replaceTable } from './table.js';
+import { replaceTable } from './transactionTable.js';
 import { loadFormCategories, displayFormErrors, transactionFormTypeChange } from './transaction.js';
 
 window.getUpdateTransactionForm = getUpdateTransactionForm;
@@ -52,35 +52,30 @@ export async function postUpdateTransactionForm(event, updateTransactionForm, tr
     const formData = new FormData(updateTransactionForm);
     formData.append('transaction_id', transaction_id);
 
-    // await fetch("http://127.0.0.1:8000/transactions/update",
-    // {
-    //     "method": "POST",
-    //     "body": formData,
-    //     "Content-Type": "application/x-www-form-urlencoded"
-    // })
-    // .then(response => {
-    //     if (!response.ok ){
-    //         throw response.json()
-    //     }
-    //     return response.json();
-    // })
-    // .then(async (data) => {
-    //     replaceTable();
-    // })
-    // .catch(error => {
-    //     error.then(data => {
-    //         console.log(data)
-    //         if ('field_errors' in data){
-    //             const field_errors = data['field_errors'];
-    //             displayFormErrors('update-transaction-form', field_errors);
-    //         }
-    //     });
-    // }  
-    // )
-
-    event.target.closest('modal')
+    await fetch("http://127.0.0.1:8000/transactions/update",
+    {
+        "method": "POST",
+        "body": formData,
+        "Content-Type": "application/x-www-form-urlencoded"
+    })
+    .then(response => {
+        if (!response.ok ){
+            throw response.json()
+        }
+        return response.json();
+    })
+    .then(async (data) => {
+        document.querySelector('#updateTransactionFormModal .close').click();   
+        await replaceTable();
+    })
+    .catch(error => {
+        error.then(data => {
+            console.log(data)
+            if ('field_errors' in data){
+                const field_errors = data['field_errors'];
+                displayFormErrors('update-transaction-form', field_errors);
+            }
+        });
+    }  
+    )
 }
-
-document.querySelector("#update-transaction-form").addEventListener("submit", (event) => {
-    postUpdateTransactionForm(event, event.target);
-})
