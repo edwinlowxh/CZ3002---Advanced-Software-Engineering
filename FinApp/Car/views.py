852337194370ccee62361be04487d631c8f3f8ba
Edwin Low <edwin.lowxh@gmail.com>
2023-03-14 23:36:01 +0800

    Added car details

--- FinApp/Car/views.py
+++ FinApp/Car/views.py
@@ -1,5 +1,11 @@
 from django.forms import model_to_dict
+
+from django.http import JsonResponse
+
 from django.shortcuts import render, redirect, reverse
+
+from django.views.decorators.csrf import csrf_exempt
+
 from django.db.models import Q
 
 from .models import (
@@ -20,6 +26,10 @@ from Car.helper.CarHelper import(
 
 from .constants import (
     CAR_TABLE_HEADER,
+    DESTINATION_VAR,
+    FREQUENCY_VAR,
+    SOURCE_VAR,
+    TRIP_ID_VAR,
     TRIP_TABLE_HEADER
 )
 
@@ -56,91 +66,130 @@ def results_view(request):
 
 def details_view(request, pk):
     if request.user.is_authenticated:
-
-        car = Car.car_manager.get_car(pk)[0]  
-        # tripMgr = TripMgr()
-        user = request.user
-        user_trips = UserTrip.user_trip_manager.get_trips(user=user)[0]
-        trips = user_trips.trips.all()
-        mileage = user_trips.mileage
-        totalCost = calc_cost(car, mileage)
-
+        print(request.method)       
         if request.method == 'POST':  # Add trip
-            if 'Add trip' in request.POST:
-                # source = request.POST.get('source')
-                # destination = request.POST.get('destination')
-                # frequency = request.POST.get('frequency')
-                # tripMgr.addTrip(source, destination, frequency)
-                # tripMgr.addTripDB()
-                # tripMgr.addUserTripDB(user)
+            print(request.POST)
+            if 'create_trip' in request.POST:
+                source = request.POST.get('source')
+                destination = request.POST.get('destination')
+                frequency = request.POST.get('frequency')
+                distance = Trip.trip_manager.calculate_distance(source, destination)
 
-                # TODO: Take reference from code on top and implement with new TripManager.py
-                return redirect(reverse('car_details', kwargs={'pk': pk}))
-            # elif 'Update Balance Sheet' in request.POST:
-            #     financeMgr = FinanceMgr(user)
+                if distance == 0:
+                    return JsonResponse({'message': 'Failed to add trip', 'non_field_errors': 'Unable to calculate trip distance for specified source and destination. Contact Administrator'}, status=500)
+                
+                trip, mileage = UserTrip.user_trip_manager.add_trip(
+                    user=request.user, source=source, 
+                    destination=destination, frequency=frequency, distance=distance)
 
-            #     if(user.information.debt_set.all().filter(debtName="Car").exists()):
-            #         financeMgr.updateDebt(user.information.debt_set.all().filter(debtName="Car")[0].id,
-            #                               "Car",  car.currentPrice, totalCost, 0.0)
-            #     else:
-            #         financeMgr.createDebt("Car", car.currentPrice,
-            #                               totalCost, 0.0)
-
-            #     if(user.information.asset_set.all().filter(assetName="Car").exists()):
-            #         financeMgr.updateAsset(user.information.asset_set.all().filter(assetName="Car")[0].id,
-            #                                "Car", car.currentPrice, -0.10)
-            #     else:
-            #         financeMgr.createAsset(
-            #             "Car", car.currentPrice, -0.10)
-            #     #Redirect
-            #     return redirect("/../finance/balanceSheet_Result/")
+                return JsonResponse(model_to_dict(trip), status=201)
             
-            elif 'Update Balance Sheet' in request.POST:
+            elif 'update_balance_sheet' in request.POST:
                 # Create category
-                car_category = Category.category_manager.get_categories(user = user, name = "Car Mortage")
+                car_category = Category.category_manager.get_categories(user=request.user, name = "Car Mortgage")
 
                 if not car_category:
-                     category = Category.category_manager.create_category(user=user, name = "Car Mortage")
+                     category = Category.category_manager.create_category(user=request.user, name = "Car Mortgage")
                 else:
                      category = car_category[0]
+                
+                car = Car.car_manager.get_car(pk)[0]  
+                total_cost = calc_cost(car, mileage)
 
-                car_transaction = Transaction.transaction_manager.create_transaction(user = user, 
-                                                                                     amount = round(totalCost,2), 
-                                                                                     description= "rent for " + car.model + car.spec,
-                                                                                     type = "EXPENSE",
-                                                                                     category= category,
-                                                                                     date = now().today())
+                car_transaction = Transaction.transaction_manager.create_transaction(
+                    user = request.user, 
+                    amount = round(total_cost,2), 
+                    description= "rent for " + car.model + car.spec,
+                    type = "EXPENSE",
+                    category= category,
+                    date = now().today()
+                )
 
                 messages.success(request, "Car Expense sucessfully copied!" )
                 return redirect("/transactions/")
+        elif request.method =='GET':
+            car = Car.car_manager.get_car(pk)[0]  
+            user_trips = UserTrip.user_trip_manager.get_trips(user=request.user)[0]
+            trips = user_trips.trips.all()
+            mileage = user_trips.mileage
+            total_cost = calc_cost(car, mileage)
+            context={
+                'car':model_to_dict(car),
+                'trips':trips,
+                'total_mileage':mileage / 1000,
+                'total_cost': round(total_cost, 2),
+                'car_table_header': CAR_TABLE_HEADER,
+                'trip_table_header': TRIP_TABLE_HEADER
+            }
 
-                
-
-        context={
-            'car':model_to_dict(car),
-            'trips':trips,
-            'total_mileage':mileage / 1000,
-            'total_cost': round(totalCost, 2),
-            'car_table_header': CAR_TABLE_HEADER,
-            'trip_table_header': TRIP_TABLE_HEADER
-        }
-
-        return render(request, "car/details.html", context)
+            return render(request, "car/details.html", context)
     else:
-        return render(request, 'car/details.html', {})
+        return redirect('login')
 
-
-def trip_delete(request, pk):
+@csrf_exempt    
+def delete_trip(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
-            return redirect(reverse('car_details', kwargs={'pk': pk}))
-        # tripMgr = TripMgr()
-        # trips = tripMgr.getUserTripDB(request.user)
+            trip_id = request.POST.get(TRIP_ID_VAR)
+            
+            if not trip_id:
+                return JsonResponse({'message': 'Failed to delete trip', 'non_field_errors': 'Please provide a trip id'}, status=422)
 
-        # tripMgr.deleteTrip(-1)
-        # tripMgr.addUserTripDB(request.user)
-        # TODO: Take reference from code on top and implement with new TripManager.py
+            try:
+                mileage = UserTrip.user_trip_manager.delete_trip(user=request.user, trip_id=trip_id)
+                return JsonResponse({}, status=201)
+            except Exception as e:                
+                return JsonResponse({'message': 'Failed to delete trip', 'non_field_errors': 'Failed to delete trip. Contact Administrator'}, status=500)
 
-        return render(request, 'car/trip_delete.html')
-    else:
-        return render(request, 'car/trip_delete.html')
+@csrf_exempt    
+def get_trip(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            trip_id = request.POST.get(TRIP_ID_VAR)
+            print(trip_id)
+            trip = Trip.trip_manager.get_trip(id=trip_id)[0]
+
+            if not trip:
+                return JsonResponse({'message': 'Failed to retrieve trip details', 'non_field_errors': 'Failed to retrieve trip. Contact Administrator'}, status=500)
+            
+            user_trips = UserTrip.user_trip_manager.get_trips(user=request.user)[0]
+            trips = user_trips.trips.all()
+
+            if trip not in trips:
+                return JsonResponse({'message': 'Failed to retrieve trip details', 'non_field_errors': 'Failed to retrieve trip details. Contact Administrator'}, status=500)
+            else:
+                return JsonResponse({'trip': model_to_dict(trip)}, status=200)
+            
+@csrf_exempt    
+def update_trip(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            trip_id = request.POST.get(TRIP_ID_VAR)
+            trip = Trip.trip_manager.get_trip(id=trip_id)[0]
+
+            if not trip:
+                return JsonResponse({'message': 'Failed to retrieve trip details', 'non_field_errors': 'Failed to retrieve trip details. Contact Administrator'}, status=500)
+            
+            user_trips = UserTrip.user_trip_manager.get_trips(user=request.user)[0]
+            trips = user_trips.trips.all()
+
+            if trip not in trips:
+                return JsonResponse({'message': 'Failed to retrieve trip details', 'non_field_errors': 'Failed to retrieve trip details. Contact Administrator'}, status=500)
+            else:
+                source = request.POST.get(SOURCE_VAR)
+                destination = request.POST.get(DESTINATION_VAR)
+                frequency = request.POST.get(FREQUENCY_VAR)
+                distance = Trip.trip_manager.calculate_distance(source=source, destination=destination)
+
+                if distance == 0:
+                    return JsonResponse({'message': 'Failed to update trip', 'non_field_errors': 'Unable to calculate trip distance for specified source and destination'}, status=500)
+
+                trip, mileage = UserTrip.user_trip_manager.update_trip(user=request.user,
+                    trip_id=trip_id, source=source, destination=destination, frequency=frequency, distance=distance)
+
+                if not trip:
+                    return JsonResponse({'message': 'Failed to update trip', 'non_field_errors': 'Unable to update trip. Contact Administrator'}, status=500)
+
+                return JsonResponse(model_to_dict(trip), status=200)
+            
+            
